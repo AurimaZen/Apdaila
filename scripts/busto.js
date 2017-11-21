98d92ef3eec8287d1c8eef3620476836a16177a1
AurimaZen <33114121+AurimaZen@users.noreply.github.com>
2017-11-21 22:26:27 +0200

    Add files via upload

--- scripts/busto.js
+++ scripts/busto.js
@@ -49,6 +49,17 @@ $(document).ready(function(){
 		slides.eq($(this).index()).prevAll().addClass('prev');
 		slides.eq($(this).index()).nextAll().removeClass('prev');
 	});
-});   
-    
+}); 
+    /*SEARCH*/
+      function expand() {
+  $(".search").toggleClass("close");
+  $(".input").toggleClass("square");
+  if ($('.search').hasClass('close')) {
+    $('input').focus();
+  } else {
+    $('input').blur();
+  }
+}
+$('button').on('click', expand);
+
 });
